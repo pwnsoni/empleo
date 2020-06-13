7e4fb8e2e0cacb3bf684f0a2cb686a2732151252
Pawan Soni <pawansoni76@gmail.com>
2020-06-13 23:56:53 +0530

    Added boiler plate code and footer

---
+++ components/Footer.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <div id="hrLine"> </div>
+        <div id="footer">
+            <div id="footer-content">
+                <p> 
+                    Made with <font-awesome-icon :icon="['fas', 'heart']"/> by Nuxt 
+                    <br/> <a href="mailto:pawansoni76@gmail.com"> Contact us </a> 
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style  scoped>
+
+    div #hrLine{
+        width: 100%;
+        height: 1px;
+        margin-bottom: 25px;
+        position: relative;
+        background: rgb(194, 194, 241);
+    }
+    
+    a{
+        color: darkblue;
+        text-decoration: underline;
+    }
+
+    a:hover{
+        color: rgb(84, 84, 238);
+        text-decoration: none;
+    }
+
+    div #footer #footer-content {
+        margin-top: 10px;
+        margin-bottom: 30px;
+        color: darkblue;
+        justify-content: center;
+        text-align: center;
+    }
+
+
+
+
+</style>
